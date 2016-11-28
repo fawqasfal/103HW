d1c994199996981fb271cc72e632d668d9f484db
omarhegazy <omarbicboi@gmail.com>
2016-11-28 03:28:12 -0500

    fixed assignment 6 to detach and delete unneeded objects

--- src/Hegazy6.cpp
+++ src/Hegazy6.cpp
@@ -16,63 +16,51 @@ using namespace Graph_lib;
 using namespace std;
 
 //------------------------------------------------------------------------------
-
-int main () {
-    std::srand(std::time(0)); // use current time as seed for random generator
-	int rings;
-	cout << "How many rings should there be?" << endl;
-	cin >> rings;
-	if (rings < 0) {exit(1);}
-try
-{
-    Point tl(0,0);    // top-left corner of our window
-    Simple_window win(tl,500,500,"Programming Assignment #6");
-    // screen coordinate tl for top-left corner
-    // window size(600*400)
-    // title: Canvas
-
+vector<Circle*> circles;
+const int R = 200;
+bool display_circles(Simple_window& win, int rings) {
     Text t(Point(180,500), "After " + to_string(rings) + " rings generation");
     win.attach(t);
     t.set_font(Font::helvetica);
     t.set_color(Color::black);
     t.set_font_size(15);
-    int R = 200; //big radius
-    Circle* circle = new Circle(Point(250,250),R);
-    circle->set_color(rand() % 256);
-    win.attach(*circle);
-   for(int i = 0; i < rings; i++) {
-		int n = rand() % 47 + 2; //max of 48 was chosen arbitrarily
-		for(int k = 0; k < n; k++) {
-			double r = R*sin(M_PI/n)/(1 + sin(M_PI/n)); //little radii
-			double x = 250 + (200-r)*cos(k*2*M_PI/n); //x and y positions
-			double y = 250 + (200-r)*sin(k*2*M_PI/n);
-			Circle* ck = new Circle(Point(x,y),r);
-			ck->set_color(rand() % 256);
-			win.attach(*ck);
-		}
-	}
-
-    while(win.wait_for_button()) {
-    	int n = rand() % 47 + 2; //max of 48 was chosen arbitrarily
-    	for(int k = 0; k < n; k++) {
-    		double r = R*sin(M_PI/n)/(1 + sin(M_PI/n)); //little radii
-    		double x = 250 + (200-r)*cos(k*2*M_PI/n); //x and y positions
-    		double y = 250 + (200-r)*sin(k*2*M_PI/n);
-    		Circle* ck = new Circle(Point(x,y),r);
-    		ck->set_color(rand() % 256);
-    		win.attach(*ck);
-    	}
-    	rings++;
-    	t.set_label("After " + to_string(rings) + " rings generation");
+    win.attach(t);
+    for(int i = 0; i < rings; i++) {
+        int n = rand() % 47 + 2; //max of 48 was chosen arbitrarily
+        for(int k = 0; k < n; k++) {
+            double r = R*sin(M_PI/n)/(1 + sin(M_PI/n)); //little radii
+            double x = 250 + (200-r)*cos(k*2*M_PI/n); //x and y positions
+            double y = 250 + (200-r)*sin(k*2*M_PI/n);
+            Circle* ck = new Circle(Point(x,y),r);
+            ck->set_color(rand() % 256);
+            circles.push_back(ck);
+        }
     }
+    for (int i = 0; i < circles.size(); i++) {
+        win.attach(*circles[i]);
+    }
+    win.wait_for_button();
+
+    for (int i = 0; i < circles.size(); i++) {
+        win.detach(*circles[i]);
+    }
+    win.detach(t);
+    return true;
 }
-catch(exception& e) {
-    // some error reporting
-    return 1;
-}
-catch(...) {
-    // some more error reporting
-    return 2;
-}
+int main () {
+    std::srand(std::time(0)); // use current time as seed for random generator
+    int rings;
+    cout << "How many rings should there be?" << endl;
+    cin >> rings;
+    if (rings < 0) {exit(1);}
+
+    Point tl(0,0);    // top-left corner of our window
+    Simple_window win(tl,500,500,"Programming Assignment #6");
+    Circle base_circle(Point(250,250), R);
+    base_circle.set_color(rand() % 256);
+    win.attach(base_circle);
+    while(display_circles(win, rings)) rings++;
+    win.detach(base_circle);
+    for (int i = 0; i < circles.size(); i++) delete circles[i];
 }
 //------------------------------------------------------------------------------
