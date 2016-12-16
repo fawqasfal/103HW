cacbf87d9fe79e2f674f1f13a72457e706bd5039
omarhegazy <omarbicboi@gmail.com>
2016-12-16 18:44:56 -0500

    added comment

--- src/Hegazy6.cpp
+++ src/Hegazy6.cpp
@@ -12,52 +12,49 @@ using namespace std;
 //------------------------------------------------------------------------------
 
 const int R = 200;
-void display_circles(Simple_window& win, int rings, int generations, vector<Circle*> circles) {
-    Text t(Point(180,500), "After " + to_string(generations) + " rings generation");
-    win.attach(t);
+void display_circles(Simple_window& win, int rings) {
+    vector<Circle*> circles;
+    Text t(Point(180,500), "After " + to_string(rings) + " rings generation");
     t.set_font(Font::helvetica);
     t.set_color(Color::black);
     t.set_font_size(15);
-    win.set_label("Programming Assignment #6");
     win.attach(t);
-    for(int k = 0; k < rings; k++) {
-        double r = R*sin(M_PI/rings)/(1 + sin(M_PI/rings)); //little radii
-        double x = 250 + (200-r)*cos(k*2*M_PI/rings); //x and y positions
-        double y = 250 + (200-r)*sin(k*2*M_PI/rings);
-        Circle* ck = new Circle(Point(x,y),r);
-        ck->set_color(rand() % 256);
-        win.attach(*ck);
-        circles.push_back(ck);
-        }
-    win.wait_for_button();
-}
-int main () {
-    /* Note : you must quit by typing in a non-number into the input!
-     * Because wait_for_button() deliberately does not handle the case of the user pressing the red X on the window
-     * (see the documentation in the Simple_window implementation), there will be a memory leak unless you quit by typing in
-     * a non-numeral input
-     */
-    std::srand(std::time(0)); // use current time as seed for random generator
-    int rings;
-    int generations = 1;
-    vector<Circle*> circles;
-    cout << "How many rings should there be?" << endl;
-    cin >> rings;
-    if (rings < 0) {exit(1);}
-    Point tl(0,0);    // top-left corner of our window
-    Simple_window win(tl,500,500,"Programming Assignment #6");
+    win.set_label("Programming Assignment #6");
     Circle base_circle(Point(250,250), R);
     base_circle.set_color(rand() % 256);
     win.attach(base_circle);
+
+    for(int i = 0; i < rings; i++) {
+        int n = rand() % 47 + 2; //max of 48 chosen arbitrarily
+        for (int k = 0; k < n; k++) {
+            double r = R*sin(M_PI/n)/(1 + sin(M_PI/n)); //little radii
+            double x = 250 + (200-r)*cos(k*2*M_PI/n); //x and y positions
+            double y = 250 + (200-r)*sin(k*2*M_PI/n);
+            Circle* ck = new Circle(Point(x,y),r);
+            ck->set_color(rand() % 256);
+            win.attach(*ck);
+            circles.push_back(ck);
+        }
+    }
+    win.wait_for_button();
+    for (int i = 0; i < circles.size(); i++)  win.detach(*(circles[i]));
+    win.detach(base_circle);
+    for (int i = 0; i < circles.size(); i++) delete circles[i];
+}
+int main () {
+    std::srand(std::time(0)); // use current time as seed for random generator
+    int rings;
+    cout << "How many rings should there be?" << endl;
+    cin >> rings;
+    if (rings <= 0) {exit(1);}
+    Point tl(0,0);    // top-left corner of our window
+    Simple_window win(tl,500,500,"Programming Assignment #6");
     while (rings >= 0) {
-        display_circles(win, rings, generations, circles);
+        display_circles(win, rings);
         cout << "How many rings should there be?" << endl;
         cin >> rings;
-        generations++;
     }
-    win.detach(base_circle);
-    for (int i = 0; i < circles.size(); i++)  win.detach(*(circles[i]));
-    for (int i = 0; i < circles.size(); i++) delete circles[i];
     win.hide();
+    return 0;
 }
 //----
